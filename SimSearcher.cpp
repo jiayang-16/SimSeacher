93589b39c53e316137af9b2460051bb5ad0a4ac5
jiayang-16 <1291950519@qq.com>
2019-04-01 23:42:04 -0700

    finish createIndex

--- SimSearcher.cpp
+++ SimSearcher.cpp
@@ -15,9 +15,41 @@ int SimSearcher::createIndex(const char *filename, unsigned q)
 {
 	char buf[1024];
 	FILE* f = fopen(filename,"r");
+	int line = 1;
 	while(fgets(buf,1024,f) != NULL){
-		printf("%s\n", buf);
+		//printf("%s", buf);
+		int len = strlen(buf)-1;
+		string edKey;
+		string jaccardKey;
+		string str(buf);
+		str = str.substr(0,len);
+		for(int i = 0;i < len-q+1;i++){
+			edKey = str.substr(i,3);
+			edIndex[edKey].push_back(line);
+		}
+		str = str+" ";
+		for(int i = 0;i < len+1;i++){
+			int pos = str.find(" ",i);
+			if(pos < len+1){
+				jaccardKey = str.substr(i,pos-i);
+				jaccardIndex[jaccardKey].push_back(line);
+				i = pos;
+			}
+		}
+		line ++;
 	}
+	map<string,vector<int>>::iterator ite;
+	for(ite = edIndex.begin();ite!=edIndex.end();ite++){
+		edSortedList.push_back(Index(ite->first,(ite->second).size()));
+	}
+	for(ite = jaccardIndex.begin();ite!=jaccardIndex.end();ite++){
+		jaccardSortedList.push_back(Index(ite->first,(ite->second).size()));
+	}
+	sort(edSortedList.begin(),edSortedList.end(),SortFunc);
+	sort(jaccardSortedList.begin(),jaccardSortedList.end(),SortFunc);
+/*	for(int i = 0;i < edSortedList.size();i++){
+		cout<<edIndex[edSortedList[i].name].size()<<endl;
+	}*/
 	return SUCCESS;
 }
 
@@ -33,3 +65,17 @@ int SimSearcher::searchED(const char *query, unsigned threshold, vector<pair<uns
 	return SUCCESS;
 }
 
+void SimSearcher::printIndex(map<string,vector<int>> mMap){
+	map<string,vector<int>>::iterator i;
+	for(i = mMap.begin();i!=mMap.end();i++){
+		cout<<"key:"<<i->first<<" "<<"list:";
+		for(int j = 0;j < (i->second).size();j++){
+			printf("%d ", (i->second)[j]);
+		}
+		printf("\n");
+	}
+}
+
+bool SortFunc(Index a,Index b){
+	return (a.length > b.length);
+}
--- SimSearcher.h
+++ SimSearcher.h
@@ -2,9 +2,25 @@
 #include <vector>
 #include <map>
 #include <utility>
-
+#include <string.h>
+#include <stdio.h>
+#include<algorithm>
+#include <iostream>
+using namespace std;
 const int SUCCESS = 0;
 const int FAILURE = 1;
+
+struct Index
+{
+	string name;
+	int length;
+	Index(string _name,int _length){
+		name = _name;
+		length = _length;
+	}
+};
+
+bool SortFunc(Index a,Index b);
 
 class SimSearcher
 {
@@ -15,5 +31,11 @@ public:
 	int createIndex(const char *filename, unsigned q);
 	int searchJaccard(const char *query, double threshold, std::vector<std::pair<unsigned, double> > &result);
 	int searchED(const char *query, unsigned threshold, std::vector<std::pair<unsigned, unsigned> > &result);
+	void printIndex(map<string,vector<int>> mMap);	
+	map<string,vector<int>> edIndex;
+	map<string,vector<int>> jaccardIndex;
+	vector<Index> edSortedList;
+	vector<Index> jaccardSortedList;
 };
+
 
